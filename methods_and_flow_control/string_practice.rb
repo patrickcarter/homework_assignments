fd57b42ce1c76e06c22b86bba6325c124c2f99c3
patrick Carter <patrickcarter1@yahoo.com>
2014-10-07 12:03:18 -0600

    added homework 1

---
+++ methods_and_flow_control/string_practice.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/env ruby
+
+string_practice = 'this is a string to practice with'
+
+puts string_practice
+puts string_practice.capitalize
+puts string_practice.upcase
+puts string_practice.gsub("string", "'string'")
+puts "The string '#{string_practice}' has #{string_practice.length} characters"
+puts string_practice.reverse
+puts string_practice.replace('practice practice practice!')
