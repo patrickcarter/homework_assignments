491e26f75729d1baa758cac840b9e4d3a417bc81
patrick Carter <patrickcarter1@yahoo.com>
2014-10-08 22:49:51 -0600

    corrected issues

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -5,7 +5,7 @@ string_practice = 'this is a string to practice with'
 puts string_practice
 puts string_practice.capitalize
 puts string_practice.upcase
-puts string_practice.gsub("string", "'string'")
+puts string_practice.gsub("string", "'string'").capitalize
 puts "The string '#{string_practice}' has #{string_practice.length} characters"
 puts string_practice.reverse
 puts string_practice.replace('practice practice practice!')
