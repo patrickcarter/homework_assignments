4183cf3204a4f7e6b81fb57d295bd198ee93e2be
patrick Carter <patrickcarter1@yahoo.com>
2014-10-26 16:19:19 -0600

    Made changes from the rejected homework

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -16,10 +16,12 @@ puts "There are #{days_per_week} days in a week"
 seconds_per_hour = seconds_per_minute * minutes_per_hour
 seconds_per_day = seconds_per_hour * hours_per_day
 seconds_per_week = seconds_per_day * days_per_week
-puts "That means there are: \n  #{seconds_per_hour} seconds in an hour
-  #{seconds_per_day} seconds in a day \n  #{seconds_per_week} seconds in a week"
+puts "That means there are: \n  #{seconds_per_hour} seconds in an hour,
+  #{seconds_per_day} seconds in a day, \n  #{seconds_per_week} seconds in a week"
 
 age = 20
 year = seconds_per_week * 52 * age
 
-puts "That means when you turn #{age} you've been alive for #{year} seconds"
+puts "That means when you turn #{age} you've been alive for #{year} seconds,"
+puts "and if you make it to #{age * 5}, you will have lived for #{year * 5} seconds."
+puts 'Make them count!'
